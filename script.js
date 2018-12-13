2c42f68bd596c741608748bc19b944667ef38af2
AlIbay8 <43456952+AlIbay8@users.noreply.github.com>
2018-12-12 17:57:25 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,6 @@
+$("button").click(function() {
+    var phrase = $("input").val().toLowerCase();
+    if (phrase === "enter") 
+    {
+    $(".riddle").html('<a href="https://AlIbay8.github.io/End">Click here</a>');}
+});
